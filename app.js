0f188d051d1d36debc68cd37dbf2e855b6cbea7c
danielhuthhpe <daniel.huth@hpe.com>
2017-07-12 12:13:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello whaetever"
-   response.end('Hello DanielH\n');
+   response.end('Hello DanielHuth\n');
 }).listen(5000);
