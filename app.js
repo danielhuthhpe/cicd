093d8f445f54318609a5466c723e0bc28ea0a7b1
danielhuthhpe <daniel.huth@hpe.com>
2017-07-13 09:46:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Some String"
-   response.end('Good day\n');
+   response.end('Good afternoon\n');
 }).listen(5000);
